ff8601084fe3ad1295de2bcf58edf8024cc68ddc
quantopaul <127382788+quantopaul@users.noreply.github.com>
2025-06-24 10:49:46 +0530

    Update Task1.py

--- Assignment 5/Task1.py
+++ Assignment 5/Task1.py
@@ -1,6 +1,6 @@
 dict1={"Raman": 90, "Baman": 100 , "Shay": 65}
 a=(input("Enter the name of the student: "))
 if a in dict1:
-                print(f"{a}'s Marks =",dict1.get(a))
+                print(f"{a}'s Marks = {dict1.get(a)}")
 else:
         print("No one by that name is on the list.")
